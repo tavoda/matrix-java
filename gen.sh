47ac5e333efe3309b63fd7c80e28cf06d761c143
Pavel Tavoda <Pavel.Tavoda@gmail.com>
2017-07-21 10:57:00 +0200

    Readme improvements

--- gen.sh
+++ gen.sh
@@ -2,6 +2,6 @@
 # -DapiDocs=false -DmodelDocs=false
 # -DapiTests=false -DmodelTests=false
 rm -rf src/main/java/* docs/ src/test/java/org/matrix/client/api/
-java -jar ~/opt/swagger-codegen-cli.jar generate -l java -c src/main/resources/config-java.json -i $1
+java -jar src/main/resources/swagger-codegen-cli.jar generate -l java -c src/main/resources/config-java.json -i $1
 rm -rf gradle
 
