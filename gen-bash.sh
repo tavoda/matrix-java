cbf8c15210e2f548debf6be486089522745ca13b
Pavel Tavoda <Pavel.Tavoda@gmail.com>
2017-07-27 13:43:05 +0200

    Generate using maven instead of shell script

--- gen-bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -DapiDocs=false -DmodelDocs=false
-# -DapiTests=false -DmodelTests=false
-rm -rf src/main/bash/*
-java -jar src/main/resources/swagger-codegen-cli.jar generate -l bash -o src/main/bash/ -c src/main/resources/config-bash.json -i src/main/resources/matrix-new-swagger.json
